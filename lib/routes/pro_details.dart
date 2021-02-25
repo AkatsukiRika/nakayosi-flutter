12960c9e3b07154bc03e036601aa036a66c44489
dengrunming <dengrunming@bytedance.com>
2021-02-25 17:47:07 +0800

    专家号申请面板UI开发完成

--- lib/routes/pro_details.dart
+++ lib/routes/pro_details.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/widgets/controller.dart';
+import 'package:flutter_quill/widgets/editor.dart';
+import 'package:flutter_quill/widgets/toolbar.dart';
 import 'package:nakayosi_flutter/common/global.dart';
 
 class ProDetails extends StatefulWidget {
@@ -8,6 +13,16 @@ class ProDetails extends StatefulWidget {
 }
 
 class _ProDetailsState extends State<ProDetails> {
+  final QuillController _controller = QuillController.basic();
+  final FocusNode _focusNode = FocusNode();
+
+  String getEditorText() {
+    final editorText = jsonEncode(_controller.document.toDelta().toJson());
+    // TODO: Wait for backend development
+    print(editorText);
+    return editorText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +32,7 @@ class _ProDetailsState extends State<ProDetails> {
           TextButton(
             onPressed: () {
               // 上传此页面和become_pro页面的信息
+              getEditorText();
             },
             child: Text(GlobalStrings.richTextSubmit),
             style: ButtonStyle(
@@ -39,8 +55,21 @@ class _ProDetailsState extends State<ProDetails> {
         mainAxisSize: MainAxisSize.max,
         children: [
           Expanded(
-            child: Text('This is a text editor'),
+            child: QuillEditor(
+              controller: _controller,
+              readOnly: false,
+              autoFocus: false,
+              expands: false,
+              focusNode: _focusNode,
+              padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+              scrollController: ScrollController(),
+              scrollable: true,
+              placeholder: GlobalStrings.richTextHint,
+              enableInteractiveSelection: true,
+              scrollPhysics: ClampingScrollPhysics(),
+            ),
           ),
+          QuillToolbar.basic(controller: _controller),
         ],
       ),
     );
